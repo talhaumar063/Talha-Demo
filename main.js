e0d4126b5e355d141048ddf5c05afece9c277e04
talhaumar063 <tumar7161@gmail.com>
2023-11-23 02:58:10 +0530

    Update QuerySelector

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-// //Changing Name - title of the page 
+// //Changing Name - title of the page// 
 // var PageTitle = document.getElementById("1")
 // console.log(PageTitle.textContent = "Adding Items")
 
-// // Adding Black Border 
+// // Adding Black Border// 
 // var Header = document.getElementById("main-header");
 // Header.style.borderBottom = 'solid 5px #000';
 
-// // change the ADD ITEMS to Bold and color is Green
+// // change the ADD ITEMS to Bold and color is Green//
 
 // var Heading = document.getElementsByClassName("title");
 // Heading[0].style.color = 'green'
 // Heading[0].innerHTML = '<b>Add Items</b>';
 
-// //Make the list background color  and all list element to Bold
+// //Make the list background color  and all list element to Bold//
 // var items = document.getElementsByClassName('list-group-item');
 // items[2].style.backgroundColor = 'green';
 
@@ -20,10 +20,18 @@
 //     items[i].style.fontWeight = 'bold';
 // }
 
-//editing New <li> item by className and ByTagName
+//editing New <li> item by className and ByTagName//
 
-var lis = document.getElementsByClassName('list-group-item1');
-lis[0].style.backgroundColor = 'red';
+// var lis = document.getElementsByClassName('list-group-item1');
+// lis[0].style.backgroundColor = 'red';
 
-var li = document.getElementsByTagName('li');
-li[4].textContent = 'New List Item';
+// var li = document.getElementsByTagName('li');
+// li[4].textContent = 'New List Item';
+
+
+//QUERYSELECTOR AND QUERYSELECTORALL//
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+
+var ListItems = document.querySelectorAll('.list-group-item');
+ListItems[2].style.display = "none";
