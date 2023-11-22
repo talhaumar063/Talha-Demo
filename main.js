d3b4d4995029663f3b9a9e84c243b3b7afc1d488
talhaumar063 <tumar7161@gmail.com>
2023-11-23 01:59:20 +0530

    Modify GetElementByTagName

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-//Changing Name - title of the page 
-var PageTitle = document.getElementById("1")
-console.log(PageTitle.textContent = "Adding Items")
+// //Changing Name - title of the page 
+// var PageTitle = document.getElementById("1")
+// console.log(PageTitle.textContent = "Adding Items")
 
-// Adding Black Border 
-var Header = document.getElementById("main-header");
-Header.style.borderBottom = 'solid 5px #000';
+// // Adding Black Border 
+// var Header = document.getElementById("main-header");
+// Header.style.borderBottom = 'solid 5px #000';
 
-// change the ADD ITEMS to Bold and color is Green
+// // change the ADD ITEMS to Bold and color is Green
 
-var Heading = document.getElementsByClassName("title");
-Heading[0].style.color = 'green'
-Heading[0].innerHTML = '<b>Add Items</b>';
+// var Heading = document.getElementsByClassName("title");
+// Heading[0].style.color = 'green'
+// Heading[0].innerHTML = '<b>Add Items</b>';
 
-//Make the list background color  and all list element to Bold
-var items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = 'green';
+// //Make the list background color  and all list element to Bold
+// var items = document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor = 'green';
 
-for(var i=0; i < items.length; i++){
-    items[i].style.fontWeight = 'bold';
-}
+// for(var i=0; i < items.length; i++){
+//     items[i].style.fontWeight = 'bold';
+// }
 
 //editing New <li> item by className and ByTagName
 
